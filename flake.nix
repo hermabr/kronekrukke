5b02aaa1db97f4a75b53813f9792262dd3f6e5e3
Herman Brunborg <herman@brunborg.com>
2023-04-30 20:34:45 +0200

    Adding flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Kronekrukke";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          pscale
+          # nodejs-slim
+          nodePackages.pnpm
+          nodePackages.npm
+          nodePackages.prisma
+        ];
+        shellHook = with pkgs; ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          export DATABASE_URL="mysql://root@127.0.0.1:3309/kronekrukke"
+        '';
+      };
+    });
+}
