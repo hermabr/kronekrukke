98663e5d91acc28ef15c2ea0a840c5b418ae7f1d
Herman Brunborg <herman@brunborg.com>
2023-05-01 21:46:00 +0200

    Adding leaderboard

--- src/routes/toppliste/+page.server.ts
+++ src/routes/toppliste/+page.server.ts
@@ -3,6 +3,18 @@ import prisma from "$lib/prisma";
 
 export const load: PageServerLoad = async () => {
   return {
-    fees: await prisma.fee.findMany(),
+    leaderboard: await prisma.$queryRaw`SELECT R.id, R.name, R.count,
+       (SELECT COUNT(*) FROM 
+        (SELECT U.id, U.name, COUNT(*) AS count
+         FROM Fee F
+         JOIN User U ON F.userId = U.id
+         GROUP BY userId, name) AS T
+        WHERE T.count > R.count) + 1 AS userRank
+FROM
+  (SELECT U.id AS id, U.name AS name, COUNT(*) AS count
+   FROM Fee F
+   JOIN User U ON F.userId = U.id
+   GROUP BY userId, name) AS R
+ORDER BY R.count DESC;`,
   };
 };
--- src/routes/toppliste/+page.svelte
+++ src/routes/toppliste/+page.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  $: ({ leaderboard } = data);
 </script>
 
 <div class="m-3">
   <button on:click={() => goto("/", { invalidateAll: true })}>
     <h1 class="py-2 text-4xl font-bold text-left text-main">Toppliste</h1>
   </button>
-  <main>FOlk</main>
+  <table class="w-full mt-5 overflow-hidden border-collapse rounded">
+    <thead class="text-white bg-blue-500">
+      <tr>
+        <th class="px-4 py-2 text-left">Plass</th>
+        <th class="px-4 py-2 text-left">Navn</th>
+        <th class="px-4 py-2 text-left">Poeng</th>
+      </tr>
+    </thead>
+    <tbody class="text-white">
+      {#each leaderboard as { userRank, name, count }}
+        <tr class="bg-gray-600 odd:bg-gray-700 hover:bg-gray-400">
+          <td class="px-4 py-2">{userRank}</td>
+          <td class="px-4 py-2">{name}</td>
+          <td class="px-4 py-2">{count}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 </div>
